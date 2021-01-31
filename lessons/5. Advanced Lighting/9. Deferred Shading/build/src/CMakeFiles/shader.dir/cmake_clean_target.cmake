3fd9a6767e1f537abad7d268b8207fc18b733b8e
TheRetikGM <theretikgm@gmail.com>
2021-01-31 19:48:09 +0100

    Odstraneni build slozky

--- lessons/5. Advanced Lighting/9. Deferred Shading/build/src/CMakeFiles/shader.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libshaderd.a"
-)
--- lessons/5. Advanced Lighting/9. Deferred Shading/build/src/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define REPO_ROOT "/home/kuba/Desktop/git/opengl-learning/lessons/5. Advanced Lighting/9. Deferred Shading/../../.."
-#define PROJECT_DIR "/home/kuba/Desktop/git/opengl-learning/lessons/5. Advanced Lighting/9. Deferred Shading"
-#define SOURCE_DIR "/home/kuba/Desktop/git/opengl-learning/lessons/5. Advanced Lighting/9. Deferred Shading/src"
-#define BINARY_DIR "/home/kuba/Desktop/git/opengl-learning/lessons/5. Advanced Lighting/9. Deferred Shading/build/src"
-#define DEBUG
